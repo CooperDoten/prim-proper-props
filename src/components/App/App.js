6fa998ca8122054399172b0529811cec65977676
CooperDoten <cooper.doten@gmail.com>
2020-09-17 19:49:47 -0500

    added DinnerSupplies.js and fixed bugs from pull requests/merge conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import GuestList from '../GuestList/GuestList';
-
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies'
 import GuestForm from '../GuestForm/GuestForm';
-
-=======
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 
@@ -49,57 +47,12 @@ class App extends Component {
         {this.state.guestList[0] && <h3>{this.state.guestList[0].name}</h3>}
  feature-guest-list
         <h2>Add a new guest</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-          </label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={this.state.newGuest.name}
-            onChange={this.handleChangeFor('name')}
-          />
-          <div>
-            Would this guest like a kid's meal?
-            <div onChange={this.handleChangeFor('kidsMeal')}>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value="yes"
-                    name="kidsMeal"
-                  />
-                  Yes, this guest would like a Kid's Meal
-                </label>
-              </div>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value="no"
-                    defaultChecked
-                    name="kidsMeal"
-                  />
-                  No, this guest would not like a Kid's Meal
-                </label>
-              </div>
-            </div>
-          </div>
-          <button type="submit">Add Guest</button>
-        </form>
+        <GuestForm newGuest={this.state.newGuest}
+        handleChangeFor={this.handleChangeFor}
+        handleSubmit={this.handleSubmit}
+      />
         <GuestList guests={this.state.guestList} />
-
-
-        <h2>Dinner Supplies</h2>
-        <div>
-          Spoons: {this.state.guestList.length * 2}
-        </div>
-        <div>
-          Forks: {this.state.guestList.length * 2}
-        </div>
-        <div>
-          Knives: {this.state.guestList.length * 2}
-        </div>
+        <DinnerSupplies count={this.state.guestList.length}/>
         <Footer />
       </div>
     );
